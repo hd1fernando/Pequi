95f4ead4b35299fbc405e4cb245b77efacd56105
Fernando Gonçalves <hd1.fernandosouza@gmail.com>
2024-07-17 19:08:06 -0300

    feat: add validator for digits

--- src/cpf.zig
+++ src/cpf.zig
@@ -1,16 +1,42 @@
 const std = @import("std");
 
+pub fn main() !void {
+    _ = is_valid("30877598134");
+}
+
 pub fn is_valid(cpf: []const u8) bool {
     if (is_only_digits(cpf) == false)
         return false;
     if (has_valid_size(cpf) == false)
         return false;
-
     const new_cpf = remove_mask(cpf);
     if (has_only_same_digits(new_cpf))
         return false;
-
+    if (fist_digit_is_valid(new_cpf) == false)
+        return false;
     return true;
+}
+
+fn fist_digit_is_valid(cpf: []const u8) bool {
+    var result: i32 = 0;
+    var muiltiplier: i32 = 10;
+    for (cpf) |elem| {
+        std.debug.print("elem {d}\n", .{@as(i32, elem)});
+        std.debug.print("calculating {d} * {d} = {d}\n", .{ @as(i32, elem - 48), @as(i32, muiltiplier), @as(i32, elem - 48) * @as(i32, muiltiplier) });
+        result += @as(i32, elem - 48) * @as(i32, muiltiplier);
+        muiltiplier -= 1;
+        if (muiltiplier < 2) {
+            break;
+        }
+    }
+    std.debug.print("result  is {d}\n", .{result});
+    const result_times_ten: i32 = result * @as(i32, 10);
+    var mod: i32 = @rem(result_times_ten, @as(i32, 11));
+    std.debug.print("mod is {d}\n", .{mod});
+    std.debug.print("9 is {d}\n", .{cpf[9] - 48});
+    if (mod == 10)
+        mod = 0;
+    return (cpf[9] - 48) == mod;
 }
 
 fn has_only_same_digits(cpf: []const u8) bool {
--- src/cpf_test.zig
+++ src/cpf_test.zig
@@ -2,18 +2,18 @@ const std = @import("std");
 const expect = std.testing.expect;
 const cpf = @import("cpf.zig");
 
-test "cpf 80123439086 should be valid" {
-    const is_valid = cpf.is_valid("80123439086");
+test "cpf 30877598134 should be valid" {
+    const is_valid = cpf.is_valid("30877598134");
     try expect(is_valid == true);
 }
 
 test "cpf with non digit should be invalid" {
-    const is_valid = cpf.is_valid("a");
+    const is_valid = cpf.is_valid("961.260.812-18");
     try expect(is_valid == false);
 }
 
 test "cpf with dot or trace should be valid" {
-    const is_valid = cpf.is_valid("833.668.880-32");
+    const is_valid = cpf.is_valid("801.234.390-86");
     try expect(is_valid == true);
 }
 
@@ -29,5 +29,10 @@ test "cpf with more than 11 digits should be invalid" {
 
 test "cpf with only same digits should be invalid" {
     const is_valid = cpf.is_valid("111.111.111-11");
+    try expect(is_valid == false);
+}
+
+test "cpf should be invalid  when first digit is invalid" {
+    const is_valid = cpf.is_valid("149.764.610-00");
     try expect(is_valid == false);
 }
