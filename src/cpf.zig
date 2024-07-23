ec31f49b5910e165d6397a6954176e03e849bd0e
Fernando Gonçalves <hd1.fernandosouza@gmail.com>
2024-07-22 23:31:47 -0300

    refactor: change cpf validator to cpf.zig

---
+++ src/cpf.zig
@@ -0,0 +1,100 @@
+const std = @import("std");
+
+pub fn is_valid(cpf: []const u8) !bool {
+    if (is_only_digits(cpf) == false)
+        return false;
+    if (has_valid_size(cpf) == false)
+        return false;
+    const new_cpf = try remove_mask(cpf);
+
+    if (has_only_same_digits(new_cpf))
+        return false;
+    if (fist_digit_is_valid(new_cpf) == false)
+        return false;
+    if (second_digit_is_valid(new_cpf) == false)
+        return false;
+    return true;
+}
+
+fn second_digit_is_valid(cpf: []const u8) bool {
+    var result: i32 = 0;
+    var multiplier: i32 = 11;
+    for (cpf) |elem| {
+        result += @as(i32, elem - 48) * @as(i32, multiplier);
+        multiplier -= 1;
+        if (multiplier < 2)
+            break;
+    }
+    var mod: i32 = @rem(result * 10, 11);
+    if (mod == 10)
+        mod = 0;
+    return (cpf[10] - 48) == mod;
+}
+
+fn fist_digit_is_valid(cpf: []const u8) bool {
+    var result: i32 = 0;
+    var multiplier: i32 = 10;
+    for (cpf) |elem| {
+        result += @as(i32, elem - 48) * @as(i32, multiplier);
+        multiplier -= 1;
+        if (multiplier < 2)
+            break;
+    }
+    const result_times_ten: i32 = result * @as(i32, 10);
+    var mod: i32 = @rem(result_times_ten, @as(i32, 11));
+    if (mod == 10)
+        mod = 0;
+    return (cpf[9] - 48) == mod;
+}
+
+fn has_only_same_digits(cpf: []const u8) bool {
+    var buff: u8 = cpf[0];
+    for (cpf) |elem| {
+        if (elem != buff)
+            return false;
+        buff = elem;
+    }
+    return true;
+}
+
+fn remove_mask(cpf: []const u8) ![]u8 {
+    var new_cpf: [11]u8 = undefined;
+    var counter: usize = 0;
+    for (cpf) |elem| {
+        if (is_digit(elem)) {
+            new_cpf[counter] = elem;
+            counter += 1;
+        }
+    }
+    const cpf_copy = try std.heap.page_allocator.dupe(u8, &new_cpf);
+    return cpf_copy;
+}
+
+fn is_only_digits(cpf: []const u8) bool {
+    for (cpf) |elem| {
+        if (elem == 0x2d)
+            continue;
+        if (elem == 0x2e)
+            continue;
+        if (is_digit(elem) == false)
+            return false;
+    }
+    return true;
+}
+
+fn is_digit(char: u8) bool {
+    if (char < 0x30)
+        return false;
+    if (char > 0x39)
+        return false;
+    return true;
+}
+
+fn has_valid_size(cpf: []const u8) bool {
+    var counter: i32 = 0;
+    for (cpf) |elem| {
+        if (is_digit(elem))
+            counter += 1;
+    }
+    return counter == 11;
+}
--- src/root.zig
+++ src/root.zig
@@ -1,141 +1,43 @@
 const std = @import("std");
 const expect = std.testing.expect;
-
-pub fn is_valid(cpf: []const u8) !bool {
-    if (is_only_digits(cpf) == false)
-        return false;
-    if (has_valid_size(cpf) == false)
-        return false;
-    const new_cpf = try remove_mask(cpf);
-
-    if (has_only_same_digits(new_cpf))
-        return false;
-    if (fist_digit_is_valid(new_cpf) == false)
-        return false;
-    if (second_digit_is_valid(new_cpf) == false)
-        return false;
-    return true;
-}
-
-fn second_digit_is_valid(cpf: []const u8) bool {
-    var result: i32 = 0;
-    var multiplier: i32 = 11;
-    for (cpf) |elem| {
-        result += @as(i32, elem - 48) * @as(i32, multiplier);
-        multiplier -= 1;
-        if (multiplier < 2)
-            break;
-    }
-    var mod: i32 = @rem(result * 10, 11);
-    if (mod == 10)
-        mod = 0;
-    return (cpf[10] - 48) == mod;
-}
-
-fn fist_digit_is_valid(cpf: []const u8) bool {
-    var result: i32 = 0;
-    var multiplier: i32 = 10;
-    for (cpf) |elem| {
-        result += @as(i32, elem - 48) * @as(i32, multiplier);
-        multiplier -= 1;
-        if (multiplier < 2)
-            break;
-    }
-    const result_times_ten: i32 = result * @as(i32, 10);
-    var mod: i32 = @rem(result_times_ten, @as(i32, 11));
-    if (mod == 10)
-        mod = 0;
-    return (cpf[9] - 48) == mod;
-}
-
-fn has_only_same_digits(cpf: []const u8) bool {
-    var buff: u8 = cpf[0];
-    for (cpf) |elem| {
-        if (elem != buff)
-            return false;
-        buff = elem;
-    }
-    return true;
-}
-
-fn remove_mask(cpf: []const u8) ![]u8 {
-    var new_cpf: [11]u8 = undefined;
-    var counter: usize = 0;
-    for (cpf) |elem| {
-        if (is_digit(elem)) {
-            new_cpf[counter] = elem;
-            counter += 1;
-        }
-    }
-    const cpf_copy = try std.heap.page_allocator.dupe(u8, &new_cpf);
-    return cpf_copy;
-}
-
-fn is_only_digits(cpf: []const u8) bool {
-    for (cpf) |elem| {
-        if (elem == 0x2d)
-            continue;
-        if (elem == 0x2e)
-            continue;
-        if (is_digit(elem) == false)
-            return false;
-    }
-    return true;
-}
-
-fn is_digit(char: u8) bool {
-    if (char < 0x30)
-        return false;
-    if (char > 0x39)
-        return false;
-    return true;
-}
-
-fn has_valid_size(cpf: []const u8) bool {
-    var counter: i32 = 0;
-    for (cpf) |elem| {
-        if (is_digit(elem))
-            counter += 1;
-    }
-    return counter == 11;
-}
+const cpf = @import("cpf.zig");
 
 test "cpf 30877598134 should be valid" {
-    const result = try is_valid("30877598134");
+    const result = try cpf.is_valid("30877598134");
     try expect(result == result);
 }
 
 test "cpf with non digit should be invalid" {
-    const result = try is_valid("961.260.812-1a");
+    const result = try cpf.is_valid("961.260.812-1a");
     try expect(result == false);
 }
 
 test "cpf with dot or trace should be valid" {
-    const result = try is_valid("801.234.390-86");
+    const result = try cpf.is_valid("801.234.390-86");
     try expect(result == true);
 }
 
 test "cpf with less than 11 digits should be invalid" {
-    const result = try is_valid("8012343908") == true;
+    const result = try cpf.is_valid("8012343908") == true;
     try expect(result == false);
 }
 
 test "cpf with more than 11 digits should be invalid" {
-    const result = try is_valid("801234390811");
+    const result = try cpf.is_valid("801234390811");
     try expect(result == false);
 }
 
 test "cpf with only same digits should be invalid" {
-    const result = try is_valid("111.111.111-11");
+    const result = try cpf.is_valid("111.111.111-11");
     try expect(result == false);
 }
 
 test "cpf should be invalid  when first digit is invalid" {
-    const result = try is_valid("149.764.610-00");
+    const result = try cpf.is_valid("149.764.610-00");
     try expect(result == false);
 }
 
 test "cpf should be invalid when second digit is invalid" {
-    const result = try is_valid("30877598130");
+    const result = try cpf.is_valid("30877598130");
     try expect(result == false);
 }
