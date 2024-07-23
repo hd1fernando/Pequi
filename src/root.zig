be91fe58bef8ffc56a8d13890dfbab6c1b5ceb0a
Fernando Gonçalves <hd1.fernandosouza@gmail.com>
2024-07-22 23:08:11 -0300

    refactor: chage project type to zig init

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
-
-pub fn main() !void {
-    _ = try is_valid("30877598134");
-}
+const expect = std.testing.expect;
 
 pub fn is_valid(cpf: []const u8) !bool {
     if (is_only_digits(cpf) == false)
@@ -101,4 +98,44 @@ fn has_valid_size(cpf: []const u8) bool {
             counter += 1;
     }
     return counter == 11;
+}
+
+test "cpf 30877598134 should be valid" {
+    const result = try is_valid("30877598134");
+    try expect(result == result);
+}
+
+test "cpf with non digit should be invalid" {
+    const result = try is_valid("961.260.812-1a");
+    try expect(result == false);
+}
+
+test "cpf with dot or trace should be valid" {
+    const result = try is_valid("801.234.390-86");
+    try expect(result == true);
+}
+
+test "cpf with less than 11 digits should be invalid" {
+    const result = try is_valid("8012343908") == true;
+    try expect(result == false);
+}
+
+test "cpf with more than 11 digits should be invalid" {
+    const result = try is_valid("801234390811");
+    try expect(result == false);
+}
+
+test "cpf with only same digits should be invalid" {
+    const result = try is_valid("111.111.111-11");
+    try expect(result == false);
+}
+
+test "cpf should be invalid  when first digit is invalid" {
+    const result = try is_valid("149.764.610-00");
+    try expect(result == false);
+}
+
+test "cpf should be invalid when second digit is invalid" {
+    const result = try is_valid("30877598130");
+    try expect(result == false);
 }
